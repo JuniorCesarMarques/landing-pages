74b505338ec95e3031e7f933016f6083793a0abe
JuniorCesarMarques <juniorcms1995@gmail.com>
2024-10-07 17:46:05 -0300

    first commit

---
+++ scripts/faqData.js
@@ -0,0 +1,28 @@
+const faqData = [
+    {
+        question: "Pergunta 1",
+        answer: "Resposta 1"
+    },
+    {
+        question: "Pergunta 2",
+        answer: "Answer 2"
+    },
+    {
+        question: "Pergunta 3",
+        answer: "Answer 3"
+    },
+    {
+        question: "Pergunta 4",
+        answer: "Answer 4"
+    },
+    {
+        question: "Pergunta 5",
+        answer: "Answer 5"
+    },
+    {
+        question: "Pergunta 6",
+        answer: "Answer 6"
+    }
+]
+
+export default faqData;
